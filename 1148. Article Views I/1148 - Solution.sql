e940194d5b25ab76ba9716bfc9e61a0fadfd027a
Michael Getu <michaelgetuk@yahoo.com>
2024-08-04 22:30:13 +0300

    Create 1148 - Solution.sql

---
+++ 1148. Article Views I/1148 - Solution.sql
@@ -0,0 +1,2 @@
+select DISTINCT author_id as id from Views where author_id=viewer_id ORDER BY author_id ASC;
+-- Key: Use 'DISTINCT' to select unique values, Use 'as' to give display name for column, and Use 'ORDER BY' to specify ordering column
