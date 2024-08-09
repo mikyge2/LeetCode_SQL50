03cab269026aea1e1cda394daad40cacdc2d748c
Michael Getu <michaelgetuk@yahoo.com>
2024-08-09 23:15:11 +0300

    Update 1280 - Solution.sql

--- 1280. Students and Examinations/1280 - Solution.sql
+++ 1280. Students and Examinations/1280 - Solution.sql
@@ -14,3 +14,7 @@ GROUP BY
 ORDER BY 
     S.student_id, Su.subject_name;
 -- Key: The use of cross join and Grouping via multiple columns
+/* A CROSS JOIN produces a Cartesian product of two tables, meaning every row in the first table is combined with every row in the second table.
+A LEFT JOIN returns all records from the left table (Students S cross-joined with Subjects Su), and the matched records from the right table (Examinations E). If there's no match, the result is NULL on the right side.
+This join checks if a student (S.student_id) has attended an exam for a particular subject (Su.subject_name). If no such exam exists, the E.subject_name will be NULL.
+Grouping by these three columns allows the query to aggregate data for each combination of student and subject, which is necessary for the COUNT function to work correctly. */
